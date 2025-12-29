24bf37cf8dff722d8ea3be8b16625619119b0887
Ajit Singadi <ajitsingadi@gmail.com>
2025-12-29 17:18:39 +0530

    Landing page Render

---
+++ src/unAuth/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1 className="landing-title">Hello, Landing Page!</h1>
+      <p className="landing-description">Welcome to the Tubestamp App. Explore and enjoy!</p>
+    </div>
+  );
+};
+
+export default LandingPage;
